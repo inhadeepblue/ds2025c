1d77096068826a668aa9865e6f0989e868d5872c
deepblue <deepblue@inha.ac.kr>
2025-03-24 14:56:28 +0900

    v0.8 array

--- week03.py
+++ week03.py
@@ -1,6 +1,3 @@
-groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
-# ratings = [1, 2, 4, 3, 100]
-ratings = [1, 2, 4, 3]
-
-group_rating = list(zip(groups, ratings))
-print(group_rating)
+cities = ['Suwon', 'Hwasung', 'Incheon', 'Incheon', 'Bucheon', 'Incheon', 'Seoul']
+cities = set(cities)
+print(cities)
