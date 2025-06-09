be1e2a0b5bd5ad8bdfc87b50906fbcb8102d30ce
deepblue <deepblue@inha.ac.kr>
2025-06-09 14:13:59 +0900

    v5.4 boj 1929

--- week14.py
+++ week14.py
@@ -1,8 +1,14 @@
+import math
+
 def is_prime(n) -> bool:
   if n <= 1:
       return False
+  elif n == 2:
+      return True
+  elif n % 2 == 0:
+      return False
   else:
-    for i in range(2, n):
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
         if n % i == 0:
           return False
   return True
